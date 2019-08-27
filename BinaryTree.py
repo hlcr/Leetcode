c06ce7a87bfc5720d47e91c237bcacb26994fb7b
Hongli <hongliryan@gmail.com>
2019-08-27 12:34:26 -0700

    binarytree, huffman

---
+++ BinaryTree.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Traversal(object):
+    def preorder(self, root):
+        if root:
+            print(root.val, end=" ")
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            print(root.val, end=" ")
+            self.inorder(root.right)
+
+    def postorder(self, root):
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            print(root.val, end=" ")
+
+if __name__ == '__main__':
+    t1 = TreeNode(1)
+    t2 = TreeNode(2)
+    t3 = TreeNode(3)
+    t4 = TreeNode(4)
+    t5 = TreeNode(5)
+    t6 = TreeNode(6)
+    t7 = TreeNode(7)
+
+    t1.left = t2
+    t1.right = t3
+    t2.left = t4
+    t2.right = t5
+    t3.left = t6
+    t3.right = t7
+
+    Traversal().preorder(t1)
+    print()
+    Traversal().inorder(t1)
+    print()
+    Traversal().postorder(t1)
