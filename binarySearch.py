08d523147168b3b1a04b3b22e4169ec723d02ab5
Hongli <hongliryan@gmail.com>
2019-09-23 14:27:54 -0700

    LeetCode

--- binarySearch.py
+++ binarySearch.py
@@ -11,7 +11,20 @@ def findLowerBound(target, nums):
             up = mid
     return bottom + 1
 
+def binary_search(target, nums):
+    i = -1
+    j = len(nums)
+    while i + 1 < j:
+        mid = i + (j - i) // 2
+        if nums[mid] < target:
+            i = mid
+        else:
+            j = mid
+    return i +1
+
+
+
 if __name__ == '__main__':
-    sortedArray = [0,1]
-    r = findLowerBound(1, sortedArray)
+    sortedArray = [0,1,1,2,3,4,5]
+    r = binary_search(2, sortedArray)
     print(r)
